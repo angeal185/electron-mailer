1454fade55ea1eb64e6fe0a48bf425b6dba65fe2
angeal185 <beneaves01@hotmail.com>
2018-05-06 13:09:03 +0800

    update

--- main.js
+++ main.js
@@ -68,7 +68,6 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
-  $('body').append('<h3>working</h3>')
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
